4f9865cafa145b19138a82a437d522b68e2b4a58
unknown <rushidorinel@gmail.com>
2026-07-01 01:14:27 +0200

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -20,7 +21,14 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body cz-shortcut-listen="true">{children}</body>
+      <body cz-shortcut-listen="true">{children}
+
+        <Script src="https://pl30147624.effectivecpmnetwork.com/6c/7a/ae/6c7aaeab5079acf8577221eb5b132d88.js" strategy="lazyOnload" />
+        <Script async="async" data-cfasync="false" src="https://pl30147623.effectivecpmnetwork.com/063736e806677d4826e8dcce849396d9/invoke.js" />
+        <div id="container-063736e806677d4826e8dcce849396d9"></div>
+
+
+      </body>
     </html>
   );
 }
